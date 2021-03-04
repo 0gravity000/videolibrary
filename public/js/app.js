01bcf11ed16eb4ffaf6a8e4e0f94d1385554299c
0gravity000 <0gravity000@gmail.com>
2021-03-04 19:18:58 +0900

    npm run devを実行

--- public/js/app.js
+++ public/js/app.js
@@ -50004,8 +50004,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\as\010_prg\020_laravel\amaprivideo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\as\010_prg\020_laravel\amaprivideo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\as\010_prg\020_laravel\videolibrary\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\as\010_prg\020_laravel\videolibrary\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
