98009a4d9fd83f8cc70eda18b3460501a33e9934
0gravity000 <0gravity000@gmail.com>
2021-03-05 17:02:35 +0900

    ルート画面のUIを修正

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -5,11 +5,12 @@
     <div class="row">
         <div class="col-md-8">
             <a href="/video/download">ダウンロードとDB登録</a><br>
-            <a href="/video/index">表示</a><br>
-
+            <a href="/video/index">Amazon PrimeVideo で人気作品をチェック</a><br>
+            <!--
             <a href="/video/index_toprated">高評価された作品</a><br>
             <a href="/video/index_recentlyadd">プライムビデオに最近追加された作品</a><br>
             <a href="/video/index_memberbenefits">プライム会員特典で見放題のプライムビデオ</a><br>
+            -->
         </div>
     </div>
 </div>
