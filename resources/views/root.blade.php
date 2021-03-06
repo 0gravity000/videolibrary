25770882679f78baecc6312090bc8f3f321c277d
0gravity000 <0gravity000@gmail.com>
2021-03-07 06:53:55 +0900

    ルート画面の不要なリンクを削除

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -34,8 +34,6 @@
                 <p class="card-text">
                     {{ $video->description }}
                 </p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
             </div>
         </div>
         @endforeach
--- resources/views/video_category.blade.php
+++ resources/views/video_category.blade.php
@@ -38,8 +38,6 @@
                 <p class="card-text">
                     {{ $category->video->description }}
                 </p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
             </div>
         </div>
         @endforeach
