5b6661f684620604cbbde06fe149f56df01bd8f0
0gravity000 <0gravit000@gmail.com>
2022-07-12 21:31:10 +0900

    デバッグ

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () {
             event(new CheckVideosTableSeason());
-        })->dailyAt('21:23');    //毎日4:00に実行する
+        })->dailyAt('21:33');    //毎日4:00に実行する
     }
 
     /**
