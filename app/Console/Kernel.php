3bf476fbbae2b29083593eb7a595dc29cc40304e
0gravity000 <0gravit000@gmail.com>
2022-07-12 23:14:40 +0900

    デバッグ

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () {
             event(new CheckVideosTableSeason());
-        })->dailyAt('22:55');    //毎日4:00に実行する
+        })->dailyAt('23:17');    //毎日4:00に実行する
     }
 
     /**
--- app/Listeners/StoreSeasonToVideosTable.php
+++ app/Listeners/StoreSeasonToVideosTable.php
@@ -47,9 +47,9 @@ class StoreSeasonToVideosTable
                 //DBから削除する処理を入れたい
             }
             
-            $seasons_not_listbox = "";
-            $seasons_listbox_prime = "";
-            $seasons_listbox_not_prime = "";
+            $seasons_not_listbox = array();
+            $seasons_listbox_prime = array();
+            $seasons_listbox_not_prime = array();
             //シーズン
             try {
                 //リストボックスで選択可能な場合
@@ -64,7 +64,7 @@ class StoreSeasonToVideosTable
                 //#a-page > div.av-page-desktop.avu-retail-page > div.DVWebNode-detail-atf-default-wrapper.DVWebNode > div > div > div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6 > div.av-detail-section._2OjN2t > div > span > span
                 //#a-page > div.av-page-desktop.avu-retail-page > div.DVWebNode-detail-atf-default-wrapper.DVWebNode > div > div > div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6 > div.av-detail-section._2OjN2t > div > span > span
                 //dd($crawler);
-                $seasons_not_listbox = $crawler->filter('div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6')->each(function ($node) {
+                $seasons_not_listbox[0] = $crawler->filter('div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6')->each(function ($node) {
                     //dd($node);
                     //たぶんリストボックスでない場合だとこっち
                     $tmp = $node->filter('div.av-detail-section._2OjN2t > div > span > span')->text();
@@ -77,13 +77,13 @@ class StoreSeasonToVideosTable
                     return $tmp;
                 });
             } catch (\Exception $e) {
-                $seasons_not_listbox = ""; //要検証
+                $seasons_not_listbox[0][0] = ""; //要検証
             }
             //var_dump($seasons_not_listbox);
             //dd("case1 end");
 
             try {
-                $seasons_listbox_prime = $crawler->filter('div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6')->each(function ($node) {
+                $seasons_listbox_prime[0] = $crawler->filter('div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6')->each(function ($node) {
                     //dd($node);
                     //たぶんリストボックスで選択可能でプライムビデオな場合だとこっち
                     $tmp = $node->filter('div.dv-node-dp-seasons._2yk-LG.HaWow5._3w9pRe > div > label > span._36qUej')->text();
@@ -98,13 +98,13 @@ class StoreSeasonToVideosTable
                     return $tmp;
                 });
             } catch (\Exception $e) {
-                $seasons_listbox_prime = ""; //要検証
+                $seasons_listbox_prime[0][0] = ""; //要検証
             }
             //var_dump($seasons_listbox_prime);
             //dd("case2 end");
                 
             try {
-                $seasons_listbox_not_prime = $crawler->filter('div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6')->each(function ($node) {
+                $seasons_listbox_not_prime[0] = $crawler->filter('div._3KHiTg._3et9aS.av-dp-container._18hTpx._2hb1y6')->each(function ($node) {
                     //dd($node);
                     //たぶんリストボックスで選択可能でプライムビデオでないな場合だとこっち
                     $tmp = $node->filter('div.dv-node-dp-seasons._2yk-LG.HaWow5._3w9pRe > div > label > span')->text();
@@ -117,22 +117,40 @@ class StoreSeasonToVideosTable
                     return $tmp;
                 });
             } catch (\Exception $e) {
-                $seasons_listbox_not_prime = ""; //要検証
+                $seasons_listbox_not_prime[0][0] = ""; //要検証
             }
             //var_dump($seasons_listbox_not_prime);
             //dd("case3 end");
 
             //どのケースで取得した「シーズン」かを決定する
             $seasons = "";
-            if ($seasons_not_listbox != "") {
-                $seasons = $seasons_not_listbox;
-            } elseif ($seasons_listbox_prime != "") {
-                $seasons = $seasons_listbox_prime;
-            } elseif ($seasons_listbox_not_prime != "") {
-                $seasons = $seasons_listbox_not_prime;
+            if ($seasons_not_listbox[0][0] != "") {
+                $seasons = $seasons_not_listbox[0][0];
+            } elseif ($seasons_listbox_prime[0][0] != "") {
+                $seasons = $seasons_listbox_prime[0][0];
+            } elseif ($seasons_listbox_not_prime[0][0] != "") {
+                $seasons = $seasons_listbox_not_prime[0][0];
             }
 
+            Log::debug('video_id:');
+            Log::debug($video->id);
+            Log::debug('$seasons:');
+            Log::debug($seasons);
+            //var_dump('$seasons:');
+            //var_dump($seasons);
             //DB登録処理
+            if ($seasons == "") {
+                //何もしない
+            } elseif ($video->season != $seasons) {    //要検証
+                //元のシーズンが空白 または「シーズン」文字列を含まない場合、DBを更新
+                if (($video->season == "") || (mb_strpos($video->season, "シーズン") === false)) {
+                    //DBを更新
+                    $video->season = $seasons;   //要検証
+                    $video->save();
+                }
+            }
+
+            /*
             //配列かどうかチェック
             if (!is_array($seasons)) {
                 //var_dump('$seasons:');
@@ -169,8 +187,8 @@ class StoreSeasonToVideosTable
                     }
                 }
             }
+            */
         }   //$videosループend
-
         Log::debug('StoreSeasonToVideosTable.php 取り込み 完了!!');
 
     }
